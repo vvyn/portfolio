1f47995183084c4073cf3eab3ed3824a96f71314
vvyn <vicavyn3@gmail.com>
2023-11-23 17:43:44 -0600

    add basic header, about, and projects sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-slate-100 p-4">
+        <nav className="flex justify-between px-4">
+          <h1 className="text-2xl text-indigo-950">vvyn</h1>
+          <ul className="flex items-center text-indigo-950">
+            <li className="ml-6">About</li>
+            <li className="ml-6">Projects</li>
+            <li>
+              <div
+                className="bg-gradient-to-r from-blue-500 to-indigo-700 text-white px-4 py-2 rounded-xl ml-6"
+              >
+                Resume
+                </div>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main className="">
+        <div className="w-auto py-32 bg-blue-200">
+          about
+        </div>
+        <div className="flex mt-10">
+          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 1</div>
+          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 2</div>
+          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 3</div>
+          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 4</div>
+        </div>
+      </main>
     </div>
   );
 }
