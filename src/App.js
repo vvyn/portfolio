510b0c724016bca10d972aa3483bb4ae7129edfe
vvyn <vicavyn3@gmail.com>
2023-11-24 16:42:55 -0600

    update header, add projects expirence and other sections

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
         <nav className="flex justify-between px-4">
           <h1 className="text-2xl text-indigo-950">vvyn</h1>
           <ul className="flex items-center text-indigo-950">
-            <li className="ml-6">About</li>
-            <li className="ml-6">Projects</li>
+            <li onClick={() => window.open("https://github.com/vvyn")} className="ml-6 cursor-pointer">GitHub</li>
+            <li onClick={() => window.open("https://www.linkedin.com/in/vvyn/")} className="ml-6 cursor-pointer">LinkedIn</li>
             <li>
               <div
-                className="bg-gradient-to-r from-blue-500 to-indigo-700 text-white px-4 py-2 rounded-xl ml-6"
+                className="bg-gradient-to-r from-blue-500 to-indigo-700 text-white px-4 py-2 rounded-xl ml-6 cursor-pointer"
               >
                 Resume
                 </div>
@@ -20,14 +20,37 @@ function App() {
         </nav>
       </header>
       <main className="">
-        <div className="w-auto py-32 bg-blue-200">
+        <div className="w-auto py-32 bg-blue-100">
           about
         </div>
-        <div className="flex mt-10">
-          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 1</div>
-          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 2</div>
-          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 3</div>
-          <div className="ml-10 bg-indigo-200 h-72 w-72 rounded-xl">Project 4</div>
+        <div className="mt-10">expirence</div>
+        <div className="flex">
+          <div className="py-32 bg-gray-100 rounded-xl m-10 w-1/2">JPMC</div>
+          <div className="py-32 bg-gray-100 rounded-xl m-10 w-1/2">Above & Beyond Studios, Inc.</div>
+        </div>
+        <div className="">projects</div>
+        <div className="flex mt-10 mx-10">
+          <div className="bg-indigo-100 py-32 w-1/3 rounded-xl">PawMatch</div>
+          <div className="ml-10 bg-indigo-100 py-32 w-1/3 rounded-xl">Escapade</div>
+          <div className="ml-10 bg-indigo-100 py-32 w-1/3 rounded-xl">HateDeception</div>
+        </div>
+        <div className="flex mt-10 mx-10">
+          <div className="bg-indigo-100 py-32 w-1/3 rounded-xl">AIS Website</div>
+          <div className="ml-10 bg-indigo-100 py-32 w-1/3 rounded-xl">HackAI Website</div>
+          <div className="ml-10 bg-indigo-100 py-32 w-1/3 rounded-xl">Conceal & Reveal</div>
+        </div>
+        <div className="flex mt-10 mx-10">
+          <div className="bg-indigo-100 py-32 w-1/3 rounded-xl">Kids-U: Online Form and Forgot Password Function</div>
+          <div className="ml-10 bg-indigo-100 py-32 w-1/3 rounded-xl">Foodies</div>
+          <div className="ml-10 bg-indigo-100 py-32 w-1/3 rounded-xl">OnlyRizz</div>
+        </div>
+        <div className="mt-10">other</div>
+        <div className="flex">
+          <div className="py-32 bg-gray-100 rounded-xl m-10 w-1/2">certifications: aws</div>          <div className="py-32 bg-gray-100 rounded-xl m-10 w-1/2">coding languages: js, ts, python, java, c++</div>
+        </div>
+        <div className="flex">
+          <div className="py-32 bg-gray-100 rounded-xl m-10 w-1/2">technologies: bitbucket, github</div>
+          <div className="py-32 bg-gray-100 rounded-xl m-10 w-1/2">frameworks: react, react native, ionic, angular</div>
         </div>
       </main>
     </div>
